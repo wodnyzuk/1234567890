837d1e2d5169be0791459209473393279400cfe8
Żuk <144340492+wodnyzuk@users.noreply.github.com>
2023-10-18 12:21:30 +0200

    Create Zad.py

---
+++ Zad.py
@@ -0,0 +1,85 @@
+
+#while a!=b:
+#  if a>b:
+#    a = a-b
+#  else:
+#    b=b-a
+#print("nwd jest liczba",a)
+
+#a,b = int(input()),int(input())
+#c,d = a,b 
+
+#while b!=0:
+  #a,b =b,a%b
+#print("nwd",a)
+#print("nww",c*d//a)
+
+#n = int(input("podaj liczbe"))
+#print(1)
+#for i in range(1, n//2+1):
+# if n%i == 0:
+#   print(i)
+#print(n)
+
+#nwd
+
+#a,b = int(input()), int(input())
+
+
+
+
+
+
+
+
+#ZAD1
+#a = int(input())
+
+#if a%2 == 0:
+#  print("tak")
+#else:
+#  print("Nie")
+
+
+#ZAD2
+#a,b,c = int(input()),int(input()),int(input())
+#if a**2 + b**2 == c**2:
+  #print("trojkąt prostokątny")
+#else:
+  #print("Nie jest prostokątny")
+
+
+#ZAD3
+#a=0
+#n = int(input())
+#for i in range(0,n,1):
+  #a=a+1
+  #print("to jest",a,"-te powtorzenie")
+
+
+#ZAD4
+#a=0
+#n = int(input())
+#for i in range(0,n,3):
+  #print(a)
+  
+
+
+#ZAD5
+#a =int(input())
+
+#ZAD7 
+print("jak sie masz?")
+a = str(input())
+while a
+  print("jak sie masz?")
+  a = str(input())
+#ZAD8
+#d=0
+#a = int(input())
+#for i in range(0,a,1):
+  #d=d+1
+  #while a%d != 0
+    
+    
+  
